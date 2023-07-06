349a29707062d59a15c4db37662ebc75c9eec931
IuryBenicio <iury620@gmail.com>
2023-07-06 13:04:05 -0300

    Fix: add addEventListeners

--- src/main.js
+++ src/main.js
@@ -1,14 +1,43 @@
-var sinopseContainer = document.getElementsByClassName('container__sinopse');
+var sinopseContainer = document.getElementById('sinopse');
 var heroBackground = document.getElementById('hero');
+var ampliaSection = document.getElementById('ampliaSection');
+var escondido = true;
+var tamanhoSmall = true;
 
-function showSinopse(){
-    console.log('clicou');
-    console.log(sinopseContainer);
-    if(sinopseContainer.classList.contains('-hide') && heroBackground.classList.contains('--height_full')){
-        sinopseContainer.classList.remove('-hide');
-        heroBackground.classList.remove('--height_full');
+ampliaSection.addEventListener('click',function(){
+    if(verificaSinopse() && verificaHero()){
+        mostraSinopse();
     }else{
-        sinopseContainer.classList.add('-hide');
-        heroBackground.classList.add('--height_full');
+        escondeSinopse();
     }
+})
+
+//FUNÇÕES
+
+function escondeSinopse(){
+    sinopseContainer.classList.add('-hide')
+    heroBackground.classList.add('--height_full')
+}
+
+function mostraSinopse(){
+    sinopseContainer.classList.remove('-hide')
+    heroBackground.classList.remove('--height_full')
+}
+
+function verificaSinopse(){
+    if(sinopseContainer.classList.contains('-hide')){ 
+        escondido
+    }else{
+        escondido = false;
+    }
+    return escondido;
+}
+
+function verificaHero(){
+    if(heroBackground.classList.contains('--height_full')){ 
+        tamanhoSmall;
+    }else{
+        tamanhoSmall = false;
+    }
+    return tamanhoSmall;
 }
